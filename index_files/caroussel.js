ebc6ec6ecde7cb640a2ac2242e20a66f13b3ad65
Sabrine Cheurfa <sabrinecheurfa@MacBook-Air-de-Sabrine.local>
2021-02-28 18:44:17 -0500

    essai remise finale

---
+++ index_files/caroussel.js
@@ -0,0 +1,22 @@
+        
+        
+        (function(){
+        
+        let bout1 =document.getElementById('un');
+		console.log(bout1.id);
+		let bout2 =document.getElementById('deux');
+		console.log(bout2.id);
+		let bout3 =document.getElementById('trois');
+		console.log(bout3.id);
+		let caroussel = document.querySelector('.caroussel')
+		bout1.addEventListener('mousedown', function(){
+		caroussel.style.transform = "translateX(0)"
+		})
+		bout2.addEventListener('mousedown', function(){
+		caroussel.style.transform = "translateX(-100vw)	"
+		})
+		bout3.addEventListener('mousedown', function(){
+		caroussel.style.transform = "translateX(-200vw)"
+        })
+        
+    }())
